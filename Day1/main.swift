6ca7cc82755e6d3a437a32e9d5eb1f501dc4e137
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2021-11-22 17:23:37 -0500

    Prepare project for day 1

---
+++ Day1/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Day1
+//
+//  Created by Marc-Antoine Malépart on 2021-11-22.
+//
+
+import Foundation
+import AdventOfCodeUtilities
+import ArgumentParser
+
+struct Day1: DayCommand {
+    @Argument(help: "Puzzle input path")
+    var puzzleInputPath: String
+    
+    func run() throws {
+        
+    }
+}
+
+Day1.main()
