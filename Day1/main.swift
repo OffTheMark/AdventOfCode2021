845c1167e54aac1cd099a7f609d62738b8508d39
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2021-12-01 09:13:04 -0500

    Complete day 1 parts 1 and 2

--- Day1/main.swift
+++ Day1/main.swift
@@ -8,15 +8,49 @@
 import Foundation
 import AdventOfCodeUtilities
 import ArgumentParser
+import Algorithms
 
 struct Day1: DayCommand {
     @Argument(help: "Puzzle input path")
     var puzzleInputPath: String
     
     func run() throws {
-        let lines = try readLines()
+        let measurements = try readLines().compactMap(Int.init)
         
-        // TODO: Complete day 1 part 1
+        let numberOfIncreases = part1(with: measurements)
+        printTitle("Part 1", level: .title1)
+        print("Number of increases:", numberOfIncreases, terminator: "\n\n")
+        
+        let numberOfWindowIncreases = part2(with: measurements)
+        printTitle("Part 2", level: .title1)
+        print("Number of increases", numberOfWindowIncreases)
+    }
+    
+    func part1(with measurements: [Int]) -> Int {
+        let numberOfIncreases = measurements.windows(ofCount: 2)
+            .reduce(into: 0, { result, window in
+                let window = Array(window)
+                if window[1] > window[0] {
+                    result += 1
+                }
+            })
+        
+        return numberOfIncreases
+    }
+    
+    func part2(with measurements: [Int]) -> Int {
+        let windowsOfSize3 = measurements.windows(ofCount: 3)
+        let numberOfIncreases = windowsOfSize3.windows(ofCount: 2)
+            .reduce(into: 0, { result, pair in
+                let pair = Array(pair)
+                let leftSum = pair[0].reduce(0, +)
+                let rightSum = pair[1].reduce(0, +)
+                
+                if rightSum > leftSum {
+                    result += 1
+                }
+            })
+        return numberOfIncreases
     }
 }
 
