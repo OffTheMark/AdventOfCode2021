81adcc96a5fe3f4f3f07861f285dd75523c762b7
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2021-12-03 09:21:00 -0500

    Simplify with dedicated Bit and BinaryNumber types

--- Day3/main.swift
+++ Day3/main.swift
@@ -14,7 +14,9 @@ struct Day3: DayCommand {
     var puzzleInputPath: String
     
     func run() throws {
-        let numbers = try readLines()
+        let numbers: [BinaryNumber] = try readLines().map({ rawValue in
+            rawValue.compactMap(Bit.init)
+        })
         
         let powerConsumption = part1(with: numbers)
         printTitle("Part 1", level: .title1)
@@ -25,8 +27,8 @@ struct Day3: DayCommand {
         print("Life support rating:", lifeSupportRating)
     }
     
-    func part1(with numbers: [String]) -> Int {
-        let bitOccurencesByOffset: [Int: [Character: Int]] = numbers
+    func part1(with numbers: [BinaryNumber]) -> Int {
+        let bitOccurencesByOffset: [Int: [Bit: Int]] = numbers
             .reduce(into: [:], { result, number in
                 for (offset, character) in number.enumerated() {
                     var bitOccurences = result[offset, default: [:]]
@@ -36,8 +38,8 @@ struct Day3: DayCommand {
                 }
             })
         
-        var rawGammaRate = ""
-        var rawEpsilonRate = ""
+        var rawGammaRate = BinaryNumber()
+        var rawEpsilonRate = BinaryNumber()
         
         for (_, bitOccurences) in bitOccurencesByOffset.sorted(by: { $0.key < $1.key }) {
             let mostCommonBit = bitOccurences
@@ -51,38 +53,37 @@ struct Day3: DayCommand {
             rawEpsilonRate.append(leastCommonBit)
         }
         
-        let gammaRate = Int(rawGammaRate, radix: 2)!
-        let epsilonRate = Int(rawEpsilonRate, radix: 2)!
+        let gammaRate = Int(rawGammaRate)
+        let epsilonRate = Int(rawEpsilonRate)
         
         return gammaRate * epsilonRate
     }
     
-    func part2(with numbers: [String]) -> Int {
+    func part2(with numbers: [BinaryNumber]) -> Int {
         let oxygenGeneratorRating = oxygenGeneratorRating(from: numbers)
         let co2ScrubberRating = co2ScrubberRating(from: numbers)
         
         return oxygenGeneratorRating * co2ScrubberRating
     }
     
-    private func oxygenGeneratorRating(from numbers: [String]) -> Int {
+    private func oxygenGeneratorRating(from numbers: [BinaryNumber]) -> Int {
         var candidates = numbers
         
         for offset in 0 ..< candidates[0].count {
             if candidates.count == 1 {
-                return Int(candidates.first!, radix: 2)!
+                return Int(candidates.first!)
             }
             
-            var bitOccurences = [Character: Int]()
-            
+            var bitOccurences = [Bit: Int]()
             for candidate in candidates {
                 let index = candidate.index(candidate.startIndex, offsetBy: offset)
                 let bit = candidate[index]
                 bitOccurences[bit, default: 0] += 1
             }
             
-            let mostCommonBit: Character
-            if bitOccurences["0"] == bitOccurences["1"] {
-                mostCommonBit = "1"
+            let mostCommonBit: Bit
+            if bitOccurences[.zero] == bitOccurences[.one] {
+                mostCommonBit = .one
             }
             else {
                 mostCommonBit = bitOccurences
@@ -96,32 +97,31 @@ struct Day3: DayCommand {
             })
             
             if candidates.count == 1 {
-                return Int(candidates.first!, radix: 2)!
+                return Int(candidates.first!)
             }
         }
         
         fatalError("Could not find oxygen rating")
     }
     
-    private func co2ScrubberRating(from numbers: [String]) -> Int {
+    private func co2ScrubberRating(from numbers: [BinaryNumber]) -> Int {
         var candidates = numbers
         
         for offset in 0 ..< candidates[0].count {
             if candidates.count == 1 {
-                return Int(candidates.first!, radix: 2)!
+                return Int(candidates.first!)
             }
             
-            var bitOccurences = [Character: Int]()
-            
+            var bitOccurences = [Bit: Int]()
             for candidate in candidates {
                 let index = candidate.index(candidate.startIndex, offsetBy: offset)
                 let bit = candidate[index]
                 bitOccurences[bit, default: 0] += 1
             }
             
-            let leastCommonBit: Character
-            if bitOccurences["0"] == bitOccurences["1"] {
-                leastCommonBit = "0"
+            let leastCommonBit: Bit
+            if bitOccurences[.zero] == bitOccurences[.one] {
+                leastCommonBit = .zero
             }
             else {
                 leastCommonBit = bitOccurences
@@ -135,11 +135,25 @@ struct Day3: DayCommand {
             })
             
             if candidates.count == 1 {
-                return Int(candidates.first!, radix: 2)!
+                return Int(candidates.first!)
             }
         }
         
         fatalError("Could not find CO2 scrubber rating")
+    }
+}
+
+enum Bit: Character {
+    case zero = "0"
+    case one = "1"
+}
+
+typealias BinaryNumber = Array<Bit>
+
+extension Int {
+    init(_ binaryNumber: BinaryNumber) {
+        let rawValue = String(binaryNumber.map(\.rawValue))
+        self.init(rawValue, radix: 2)!
     }
 }
 
