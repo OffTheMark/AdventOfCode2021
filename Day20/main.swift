49ef4051c9d0ad6048328da5edb11cf8c3216661
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2021-12-20 08:56:13 -0500

    Measure elapsed time

--- Day20/main.swift
+++ Day20/main.swift
@@ -19,12 +19,14 @@ struct Day20: DayCommand {
         let grid = Grid(rawValue: parts[1])
         
         printTitle("Part 1", level: .title1)
-        let gridEnhancedTwice = part1(grid: grid, enhancement: enhancement)
-        print("Number of lit pixels after 2 enhancements:", gridEnhancedTwice.numberOfLitPixels(), terminator: "\n\n")
+        let (gridEnhancedTwice, part1Time) = measure({ part1(grid: grid, enhancement: enhancement) })
+        print("Number of lit pixels after 2 enhancements:", gridEnhancedTwice.numberOfLitPixels())
+        print("Elapsed time: \(part1Time) seconds", terminator: "\n\n")
         
         printTitle("Part 2", level: .title1)
-        let gridEnhanced50Times = part2(grid: grid, enhancement: enhancement)
+        let (gridEnhanced50Times, part2Time) = measure({ part2(grid: grid, enhancement: enhancement) })
         print("Number of lit pixels after 50 enhancements:", gridEnhanced50Times.numberOfLitPixels())
+        print("Elapsed time: \(part2Time) seconds")
     }
     
     func part1(grid: Grid, enhancement: Enhancement) -> Grid {
@@ -38,12 +40,21 @@ struct Day20: DayCommand {
     
     func part2(grid: Grid, enhancement: Enhancement) -> Grid {
         var grid = grid
-        for step in 0 ..< 50 {
+        for _ in 0 ..< 50 {
             grid.enhance(using: enhancement)
         }
         
         return grid
     }
+}
+
+func measure<T>(_ work: () -> T) -> (result: T, time: CFAbsoluteTime) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    
+    let result = work()
+    
+    let endTime = CFAbsoluteTimeGetCurrent()
+    return (result, endTime - startTime)
 }
 
 Day20.main()
