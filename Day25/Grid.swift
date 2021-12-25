e4e9333d6c43fb22dd8b8b564d09f68f9f36dc2b
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2021-12-25 10:29:01 -0500

    Complete day 25

---
+++ Day25/Grid.swift
@@ -0,0 +1,116 @@
+//
+//  Grid.swift
+//  Day25
+//
+//  Created by Marc-Antoine Malépart on 2021-12-25.
+//
+
+import Foundation
+
+struct Grid {
+    private(set) var cucumbersByPoint: [Point: SeaCucumber]
+    let size: Size
+    
+    func movesForEastFacingCucumbers() -> Set<Move> {
+        Set(
+            cucumbersByPoint.compactMap({ point, cucumber -> Move? in
+                guard cucumber == .east else {
+                    return nil
+                }
+                
+                var end = point
+                end.x += 1
+                if end.x >= size.width {
+                    end.x = 0
+                }
+                
+                guard cucumbersByPoint[end] == nil else {
+                    return nil
+                }
+                
+                return Move(start: point, end: end)
+            })
+        )
+    }
+    
+    func movesForSouthFacingCucumbers() -> Set<Move> {
+        Set(
+            cucumbersByPoint.compactMap({ point, cucumber -> Move? in
+                guard cucumber == .south else {
+                    return nil
+                }
+                
+                var end = point
+                end.y += 1
+                if end.y >= size.height {
+                    end.y = 0
+                }
+                
+                guard cucumbersByPoint[end] == nil else {
+                    return nil
+                }
+                
+                return Move(start: point, end: end)
+            })
+        )
+    }
+    
+    mutating func apply(_ moves: Set<Move>) {
+        for move in moves {
+            guard let cucumber = cucumbersByPoint[move.start] else {
+                continue
+            }
+            
+            cucumbersByPoint.removeValue(forKey: move.start)
+            cucumbersByPoint[move.end] = cucumber
+        }
+    }
+}
+
+extension Grid {
+    init(rawValue: String) {
+        let lines = rawValue.components(separatedBy: .newlines)
+        var size = Size(width: 0, height: lines.count)
+        
+        let cucumbersByPoint: [Point: SeaCucumber] = lines.enumerated().reduce(into: [:], { result, element in
+            let (y, line) = element
+            
+            size.width = max(size.width, line.count)
+            
+            for (x, character) in line.enumerated() {
+                guard let seaCucumber = SeaCucumber(rawValue: character) else {
+                    continue
+                }
+                
+                let point = Point(x: x, y: y)
+                result[point] = seaCucumber
+            }
+        })
+        self.cucumbersByPoint = cucumbersByPoint
+        self.size = size
+    }
+}
+
+enum SeaCucumber: Character {
+    case east = ">"
+    case south = "v"
+}
+
+struct Size {
+    var width: Int
+    var height: Int
+}
+
+struct Point {
+    var x: Int
+    var y: Int
+}
+
+extension Point: Hashable {}
+
+struct Move {
+    let start: Point
+    let end: Point
+}
+
+extension Move: Hashable {}
---
+++ Day25/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  Day25
+//
+//  Created by Marc-Antoine Malépart on 2021-12-25.
+//
+
+import Foundation
+import AdventOfCodeUtilities
+import ArgumentParser
+
+struct Day25: DayCommand {
+    @Argument(help: "Puzzle input path")
+    var puzzleInputPath: String
+    
+    func run() throws {
+        let grid = Grid(rawValue: try readFile())
+        
+        printTitle("Part 1", level: .title1)
+        let firstStepOnWhichNoCucumbersMove = part1(grid: grid)
+        print("First step on which no sea cucumbers move:", firstStepOnWhichNoCucumbersMove, terminator: "\n\n")
+    }
+    
+    func part1(grid: Grid) -> Int {
+        let steps = 1...
+        var grid = grid
+        
+        return steps.first(where: { _ in
+            let movesForEastFacing = grid.movesForEastFacingCucumbers()
+            grid.apply(movesForEastFacing)
+            
+            let movesForSouthFacing = grid.movesForSouthFacingCucumbers()
+            grid.apply(movesForSouthFacing)
+            
+            return movesForEastFacing.isEmpty && movesForSouthFacing.isEmpty
+        })!
+    }
+}
+
+Day25.main()
